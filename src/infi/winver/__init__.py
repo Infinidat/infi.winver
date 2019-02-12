9bfa6885daa982fb556575587caccf9d95a755f4
Arnon Yaari <arnony@infinidat.com>
2019-02-12 16:16:33 +0200

    HPT-2361 make sure we recognize versions > 6.2. Refactor.

--- src/infi/winver/__init__.py
+++ src/infi/winver/__init__.py
@@ -1,15 +1,34 @@
 __import__("pkg_resources").declare_namespace(__name__)
 
+# version numbers in Windows are very tricky and there are several places we have to look at
+# * "GetVersionEx" API will return major and minor versions that identify Windows versions lower than
+#    6.2 (Windows 2012 / Windows 8). It will be "stuck" at 6.2 for >= Windows Server 2012 / Windows 8
+#   (unless the application has a proper manifest - which we don't rely on)
+# * Registry value "HKLM\Software\Microsoft\Windows NT\CurrentVersion\CurrentVersion" will differentiate between
+#   Windows Server 2012 / Windows 8 and Windows Server 2012 R2 / Windows 8.1 (6.3).
+#   This value will be "stuck" at 6.3 for >= Windows 2012 R2 / Windows 8.1
+# * Registry values "HKLM\Software\Microsoft\Windows NT\CurrentVersion\CurrentMajorVersion" and "CurrentMinorVersion"
+#   are available for Windows Server 2016 / Windows 10 and on. They will be "stuck" at 10 and 0 (10.0) for
+#   Windows Server 2016 / Windows 10 and on.
+# * Registry value "HKLM\Software\Microsoft\Windows NT\CurrentVersion\ReleaseId" can differentiate versions of
+#   Windows >= Server 2016 / 10. Release IDs are different for each update of Windows 10 and Windows Server 2016 and up
+# * To differentiate between the Server versions and Workstation versions (Vista/7/8/8.1/10 etc.), the "product id"
+#   from GetVersionEx will be "1" for the workstation versions, and 2 or 3 for the server versions.
+# See https://docs.microsoft.com/en-us/windows/desktop/sysinfo/operating-system-version for major/minor version list
+
 version_to_name = {
     (5, 0): 'Windows 2000',
     (5, 1): 'Windows XP',
+    (5, 2, 1): 'Windows XP',    # Windows XP spans over 5.1 and 5.2 (workstation type)
     (5, 2): 'Windows Server 2003',
+    # TODO Windows Server 2003 R2 uses the same version. We may need to read something else to recognize it
     (6, 0, 1): 'Windows Vista',
     (6, 0): 'Windows Server 2008',
     (6, 1, 1): 'Windows 7',
     (6, 1): 'Windows Server 2008 R2',
     (6, 2, 1): 'Windows 8',
     (6, 2): 'Windows Server 2012',
+    (6, 3, 1): 'Windows 8.1',
     (6, 3): 'Windows Server 2012 R2',
     # the following will *not* be used by analyze_windows_version.
     # these are for "name_to_version" (used by greater_than)
@@ -27,18 +46,25 @@ win10_release_id_to_update = {
     1809: 'October 2018 Update',
 }
 win10_server_release_id_to_update = {
+    # Windows Server 2016
     1607: 'RTM',
-    1703: 'Aniversary Update',
-    1709: 'Fall Creators Update',
-    1803: 'version 1803',
+    # the following updates are supposedly "not Windows Server 2016" but we
+    # recognize them as updates to Server 2016 nevertheless
+    1703: 'Aniversary Update',      # a.k.a "Windows Server, v1703"
+    1709: 'Fall Creators Update',   # a.k.a "Windows Server, v1709"
+    1803: 'April 2018 Update',      # a.k.a "Windows Server, v1803"
+    # Windows Server 2019
     1809: 'RTM',
 }
+# TODO supposedly there is a difference between "Windows Server, v1809"
+# and "Windows Server 2019"? Do we need to check in more places to recognize this?
+FIRST_WIN_SERVER_2019_RELEASE_ID = 1809
 
 name_to_version = {value: key for key, value in version_to_name.items()}
 
 class Windows(object):  # pylint: disable-msg=R0902,R0904
     def __init__(self):
-        from .interface import get_version_ex, get_system_info
+        from .interface import get_version_ex
         self.hyper_v = False
         self.architecture = ""
         self.service_pack = ""
@@ -46,14 +72,12 @@ class Windows(object):  # pylint: disable-msg=R0902,R0904
         self.edition = ""
         self.version = ""
         self._version_ex = get_version_ex()
-        if self._version_ex.major_version == 10:
-            self._release_id = self.get_release_id_from_registry()
-        self._system_info = get_system_info()
-        self._edition = ""
         self.analyze()
 
     def analyze(self):
-        if self._version_ex.major_version == 10:
+        self.analyze_major_minor_versions()
+        if self._major_version == 10:
+            self._release_id = self.get_release_id_from_registry()
             self.analyze_windows10_version()
             self.analyze_windows10_update()
         else:
@@ -62,24 +86,32 @@ class Windows(object):  # pylint: disable-msg=R0902,R0904
         self.analyze_windows_service_pack()
         self.analyze_windows_architecture()
 
+    def analyze_major_minor_versions(self):
+        self._major_version = self._version_ex.major_version
+        self._minor_version = self._version_ex.minor_version
+        self._product_type = self._version_ex.product_type
+        if (self._major_version, self._minor_version) == (6, 2):
+            # GetVersionEx will return 6.2 for Windows 2012 and up. See documentation above.
+            self._major_version, self._minor_version = self.get_version_from_registry()
+
     def analyze_windows_version(self):
-        version = (self._version_ex.major_version, self._version_ex.minor_version, self._version_ex.product_type)
+        version = (self._major_version, self._minor_version, self._product_type)
         self.version = version_to_name.get(version, version_to_name.get(version[:2], 'Unknown'))
 
     def analyze_windows10_version(self):
         from .constants import VER_NT_WORKSTATION
-        # Windows 10 and on, and Windows Server 2016 and on, use only "release id" to diffentiate different releases
+        # Windows 10 and on, and Windows Server 2016 and on, use only "release id" to differentiate different releases
         # product_type still determines whether this is Windows 10 or Windows Server
-        if self._version_ex.product_type == VER_NT_WORKSTATION:
+        if self._product_type == VER_NT_WORKSTATION:
             self.version = "Windows 10"
-        elif self._release_id < 1809:
+        elif self._release_id < FIRST_WIN_SERVER_2019_RELEASE_ID:
             self.version = "Windows Server 2016"
         else:
             self.version = "Windows Server 2019"
 
     def analyze_windows10_update(self):
         from .constants import VER_NT_WORKSTATION
-        if self._version_ex.product_type == VER_NT_WORKSTATION:
+        if self._product_type == VER_NT_WORKSTATION:
             release_id_to_update = win10_release_id_to_update
         else:
             release_id_to_update = win10_server_release_id_to_update
@@ -89,16 +121,16 @@ class Windows(object):  # pylint: disable-msg=R0902,R0904
         self.server_core = False
         self.hyper_v = False
         self.edition = 'Standard'
-        if self._version_ex.major_version == 5:
+        if self._major_version == 5:
             self.analyze_windows5_edition()
-        elif self._version_ex.major_version == 6:
+        elif self._major_version == 6:
             self._edition = self.get_windows6_edition()
             self.analyze_windows6_edition()
-            if self._version_ex.minor_version >= 2:
+            if self._minor_version >= 2:
                 # http://msdn.microsoft.com/en-us/library/windows/desktop/ms724358(v=vs.85).aspx
                 # PRODUCT_*_SERVER_CORE values are not returned in Windows Server 2012
                 self.analyze_server_core_according_to_registry()
-        elif self._version_ex.major_version == 10:
+        elif self._major_version == 10:
             self._edition = self.get_windows6_edition()
             self.analyze_windows6_edition()
         else:
@@ -126,9 +158,9 @@ class Windows(object):  # pylint: disable-msg=R0902,R0904
     def get_windows6_edition(self):
         from .interface import get_product_info
         return get_product_info(self._version_ex.major_version,
-                            self._version_ex.minor_version,
-                            self._version_ex.service_pack_major,
-                            self._version_ex.service_pack_minor)
+                                self._version_ex.minor_version,
+                                self._version_ex.service_pack_major,
+                                self._version_ex.service_pack_minor)
 
     def analyze_server_core_according_to_dism(self):
         # http://stackoverflow.com/questions/13065479/how-to-detect-windows-2012-core-edition-c
@@ -161,6 +193,22 @@ class Windows(object):  # pylint: disable-msg=R0902,R0904
         release_id = int(reg_folder.values_store['ReleaseId'].to_python_object())
         return release_id
 
+    def get_version_from_registry(self):
+        # see documentation about reading the major/minor versions from the registry at the top of the file
+        from infi.registry import LocalComputer
+        local_machine = LocalComputer().local_machine
+        reg_folder = local_machine[r'Software\Microsoft\Windows NT\CurrentVersion']
+        major_version = reg_folder.values_store.get('CurrentMajorVersionNumber')
+        minor_version = reg_folder.values_store.get('CurrentMinorVersionNumber')
+        if major_version and minor_version:
+            # should be 10.0
+            return major_version.to_python_object(), minor_version.to_python_object()
+        # 6.2 or 6.3
+        major_version, minor_version = reg_folder.values_store['CurrentVersion'].to_python_object().split('.')
+        return int(major_version), int(minor_version)
+
+        return major_version, minor_version
+
     def analyze_windows6_edition(self):
         from .constants import PRODUCT_SUITE_CLUSTER, PRODUCT_SUITE_DATACENTER
         from .constants import PRODUCT_SUITE_ENTERPRISE, PRODUCT_SUITE_STORAGE, PRODUCT_SUITE_STANDARD
@@ -190,8 +238,9 @@ class Windows(object):  # pylint: disable-msg=R0902,R0904
 
     def analyze_windows_architecture(self):
         from .constants import PROCESSOR_ARCHITECTURE_AMD64, PROCESSOR_ARCHITECTURE_IA64, PROCESSOR_ARCHITECTURE_INTEL
-        self.architecture = ''
-        processor = self._system_info.processor_architecture
+        from .interface import get_system_info
+        system_info = get_system_info()
+        processor = system_info.processor_architecture
         if processor == PROCESSOR_ARCHITECTURE_AMD64:
             self.architecture = 'x64'
         elif processor == PROCESSOR_ARCHITECTURE_IA64:
@@ -268,10 +317,10 @@ class Windows(object):  # pylint: disable-msg=R0902,R0904
     def greater_than(self, os_name):
         if os_name not in name_to_version:
             raise Exception("Could not find OS name {}".format(os_name))
-        if self._version_ex.major_version == 10:
-            actual_version = (self._version_ex.major_version, self._version_ex.minor_version, self._release_id)
+        if self._major_version == 10:
+            actual_version = (self._major_version, self._minor_version, self._release_id)
         else:
-            actual_version = (self._version_ex.major_version, self._version_ex.minor_version)
+            actual_version = (self._major_version, self._minor_version)
         compared_version = name_to_version[os_name]
         if compared_version[0] < 10:
             # in name_to_version, values of Windows 10 have the release id in position 2
